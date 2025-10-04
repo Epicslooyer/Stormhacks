21885d3c7bc714f5e9799dce1ceef30cad31bf93
teeverc <72298507+teeverc@users.noreply.github.com>
2025-10-04 13:51:20 -0700

    add model preset

--- lib/openrouter.ts
+++ lib/openrouter.ts
@@ -3,3 +3,9 @@ import { createOpenRouter } from "@openrouter/ai-sdk-provider";
 export const openrouter = createOpenRouter({
 	apiKey: process.env.OPENROUTER_API_KEY,
 });
+
+export const smallModel = openrouter("openai/gpt-5-nano");
+
+export const largeModel = openrouter("openai/gpt-5");
+
+export const chatModel = openrouter("openai/gpt-5-chat");
